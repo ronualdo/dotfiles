f35e5f3351c6f80c96ed9a029e2e2eba12636a59
ronualdo maciel <raxmac@gmail.com>
2020-02-09 20:44:00 -0300

    Adds logs to zsh aliases

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -1,6 +1,7 @@
 # adds env vars for docker-compose
 alias docker-compose="CURRENT_UID=$(id -u):$(id -g) DOCKERHOST=$(ifconfig | grep -E '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1) docker-compose"
 
-alias dclog="docker-compose logs --follow"
 alias dcexec="docker-compose exec"
+alias dclog="docker-compose logs --follow"
+alias dcps="docker-compose ps"
 alias dcfull="docker-compose -f full-docker-compose.yml"
