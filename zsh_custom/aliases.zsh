64d0464a02cafec98bb75f82ad5507b089a5930a
ronualdo maciel <raxmac@gmail.com>
2020-10-24 13:04:53 -0300

    Updates tmux config
    Tmux new version changed a few of the options on its config.

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -1,5 +1,5 @@
 # adds env vars for docker-compose
-alias docker-compose="DOCKERHOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+') SSH_PRIVATE_KEY=\"$(cat ~/.ssh/id_rsa)\" docker-compose"
+# alias docker-compose="DOCKERHOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+') SSH_PRIVATE_KEY=\"$(cat ~/.ssh/id_rsa)\" docker-compose"
 
 alias dcexec="docker-compose exec"
 alias dclog="docker-compose logs --follow"
