5aa472a8042e7ca48dcdd0a4e53b3dd807ee9919
ronualdo maciel <contact@ronualdo.me>
2025-03-08 15:17:22 -0300

    Config avant with deepseek-r1:7b

--- nvim/lua/custom_config/packages.lua
+++ nvim/lua/custom_config/packages.lua
@@ -12,6 +12,64 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+avante = {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  opts = {
+    provider = "local-deepseek",
+    vendors = {
+      ["local-deepseek"] = {
+        __inherited_from = 'openai',
+        endpoint = "http://127.0.0.1:11434/v1",
+        -- model = 'deepseek/deepseek-r1',
+        model = 'deepseek-r1:7b',
+        disable_tools = true
+      }
+    }
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
+
 -- Git integration
 fugitive = "tpope/vim-fugitive"
 
@@ -74,6 +132,7 @@ luasnip = "L3MON4D3/LuaSnip"
 cmp_luasnip = "saadparwaiz1/cmp_luasnip"
 
 require("lazy").setup({
+  avante,
   cmp,
   cmp_luasnip,
   cmp_nvim,
